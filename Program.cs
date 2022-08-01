4b2b5870be591a53388a59af80b48364d4b2df7e
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-01 16:49:48 -0300

    Temperaturas

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Exercício preposto surso Nélio Alves
+// Programa vai pedir para o usuario digitar a temperatura em Celsius, logo em seguida o programa mostra na tela a temperatura
+// em Fahrenheit. Depois disso pedira se deseja repetir o precesso.
+
+double F, Temp;
+char X;
+do
+{
+    Console.Write("Digite a temperatura em Celsius:");
+    Temp = double.Parse(Console.ReadLine());
+    F = (9 * Temp / 5) + 32;
+
+    Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1"));
+    Console.Write("Deseja repetir (s / n)? ");
+    X = char.Parse(Console.ReadLine());
+    
+}while (X == 's' || X == 'S');
